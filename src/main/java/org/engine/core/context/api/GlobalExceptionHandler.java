5449bd8e53e50817cda193892c547427d626d3d4
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-27 10:07:58 -0300

    Update GlobalExceptionHandler.java

--- src/main/java/org/engine/core/context/api/GlobalExceptionHandler.java
+++ src/main/java/org/engine/core/context/api/GlobalExceptionHandler.java
@@ -1,1 +1,15 @@
+package management.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>("Erro interno: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
