43e9bcc636c30e7247e7b94268ae4773165fe2be
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-27 10:08:19 -0300

    Update RequestContext.java

--- src/main/java/org/engine/core/context/domain/RequestContext.java
+++ src/main/java/org/engine/core/context/domain/RequestContext.java
@@ -1,1 +1,26 @@
 
+package management.domain;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+import java.util.UUID;
+
+@Component
+@RequestScope
+public class RequestContext {
+
+    private String correlationId;
+
+    public RequestContext() {
+
+        this.correlationId = UUID.randomUUID().toString();
+    }
+
+    public String getCorrelationId() {
+        return correlationId;
+    }
+
+    public void setCorrelationId(String correlationId) {
+        this.correlationId = correlationId;
+    }
+}
