b051ef90f9a11d81ec5805717039a4dda3987bb5
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-27 10:09:48 -0300

    Update KafkaContextWrapper.java

--- src/main/java/org/engine/core/context/messaging/KafkaContextWrapper.java
+++ src/main/java/org/engine/core/context/messaging/KafkaContextWrapper.java
@@ -1,1 +1,23 @@
+package management.messaging;
 
+import management.infrastructure.CorrelationIdContext;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaContextWrapper {
+
+    private final CorrelationIdContext context;
+
+    public KafkaContextWrapper(CorrelationIdContext context) {
+        this.context = context;
+    }
+
+    public <K, V> void addContext(ProducerRecord<K, V> record) {
+        String correlationId = context.get();
+        if (correlationId != null) {
+            record.headers().add(new RecordHeader(CorrelationIdContext.CORRELATION_ID_KEY, correlationId.getBytes()));
+        }
+    }
+}
