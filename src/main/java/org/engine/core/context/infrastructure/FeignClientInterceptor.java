87bab93e6dbb765b0ee665c9350c547db70fdca4
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-27 10:09:15 -0300

    Update FeignClientInterceptor.java

--- src/main/java/org/engine/core/context/infrastructure/FeignClientInterceptor.java
+++ src/main/java/org/engine/core/context/infrastructure/FeignClientInterceptor.java
@@ -1,1 +1,23 @@
+package management.infrastructure;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    private final CorrelationIdContext context;
+
+    public FeignClientInterceptor(CorrelationIdContext context) {
+        this.context = context;
+    }
+
+    @Override
+    public void apply(RequestTemplate template) {
+        String correlationId = context.get();
+        if (correlationId != null) {
+            template.header(CorrelationIdContext.CORRELATION_ID_KEY, correlationId);
+        }
+    }
+}
